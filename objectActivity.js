12851dc3e2c9cbe2197305023b5f80a88395b90b
FranceLeyanneRamos <ramosfranceleyanne@gmail.com>
2024-10-20 01:05:54 +0800

    Third commit

---
+++ objectActivity.js
@@ -0,0 +1,17 @@
+//Create a car object
+const car = {
+    type: "Honda",
+    model: "Civic",
+    color: "White"
+};
+
+//Check the type of the object
+console.log(typeof car); 
+
+//Update a property of the object
+car.type = "Toyota";
+console.log(car); 
+
+//Add a new property to the object
+car.wheels = 4;
+console.log(car); 
